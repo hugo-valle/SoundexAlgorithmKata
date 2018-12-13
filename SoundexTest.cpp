f80fdfbe73947a223bba589d28e59f53d37582cb
hugovalle1 <hugovalle1@weber.edu>
2018-12-13 11:07:18 -0700

    Ensure Pads with zeros to 4 digits

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -3,10 +3,24 @@
 using namespace std;
 class Soundex
 {
+    /**
+     * @brief pad word to 4 digits
+     * @param word - input string
+     * @return - padded string
+     */
+    string zeroPad(const string& word)const
+    {
+        return word + "000";
+    }
 public:
+    /**
+     * @brief Encode string to Soundex Algorithm
+     * @param word - string to code
+     * @return - encoded string
+     */
     string encode(const string& word) const
     {
-       return word;
+       return zeroPad(word);
     }
 
 };
@@ -21,6 +35,15 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
    // Act
    auto encode = soundex.encode("A");
    // Assert
-   ASSERT_THAT(encode, Eq("A"));
+   ASSERT_THAT(encode, Eq("A000"));
 }
 
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+    // Arrange
+    Soundex soundex;
+    // Act
+    auto encode = soundex.encode("I");
+    // Assert
+    ASSERT_THAT(encode, Eq("I000"));
+}
