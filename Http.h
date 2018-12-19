d34dbc7624f81a28bf515c1b25e6bf5b049189fa
hugovalle1 <hugovalle1@weber.edu>
2018-12-19 12:38:08 -0700

    Rename exception.

---
+++ Http.h
@@ -0,0 +1,15 @@
+#ifndef FIRSTEXAMPLE_HTTPS_H
+#define FIRSTEXAMPLE_HTTPS_H
+
+#include <string>
+
+class Http
+{
+public:
+    virtual ~Http() {}
+    virtual void initialize() = 0;
+    virtual std::string get(const std::string& url) const = 0;
+};
+
+
+#endif //FIRSTEXAMPLE_HTTPS_H
