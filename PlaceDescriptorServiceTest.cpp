999ad594678f79385573dccd10246d925f5df616
hugovalle1 <hugovalle1@weber.edu>
2018-12-20 10:31:44 -0700

    Update to use MOCK Util tools

--- PlaceDescriptorServiceTest.cpp
+++ PlaceDescriptorServiceTest.cpp
@@ -21,33 +21,43 @@ const string APlaceDescriptionService::ValidLongitude("-104.44");
 // Stub Class: Returns hard Coded value
 class HttpStub: public Http
 {
-    void initialize() override {}
-    std::string get(const std::string& url)const override
-    {
-        verify(url);
-        return R"({ "address":{
-            "road":"Drury Ln",
-            "city":"Fountain",
-            "state":"CO",
-            "country":"US"} } )";
-    }
-    void verify(const std::string& url)const
-    {
-        string urlStart(
-                "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&" );
-        string expected(urlStart +
-                "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
-                "lon=" + APlaceDescriptionService::ValidLongitude);
-        ASSERT_THAT(url, EndsWith(expected));
-    }
+public:
+    // MOCK_METHOD0(,): 0 for 0 input params
+    //      firstParam: method name
+    //      secondParam: method type
+    MOCK_METHOD0(initialize, void());
+    MOCK_CONST_METHOD1(get, string(const string&));
 };
 
 TEST_F(APlaceDescriptionService, ReturnDescriptionForValidLocation)
 {
     HttpStub httpStub;  // test double
+    string urlStart(
+            "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&" );
+    string expectedURL(urlStart +
+                       "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
+                       "lon=" + APlaceDescriptionService::ValidLongitude);
+    EXPECT_CALL(httpStub, get(expectedURL));
     PlaceDescriptionService service(&httpStub);
 
-    auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
-
-    ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
+    service.summaryDescription(ValidLatitude, ValidLongitude);
 }
+
+// Stub Class: Returns hard Coded value
+//class HttpStub: public Http
+//{
+//public:
+//    std::string returnResponse;
+//    std::string expectedURL;
+//    void initialize() override {}
+//    std::string get(const std::string& url)const override
+//    {
+//        verify(url);
+//        return returnResponse;
+//    }
+//    void verify(const std::string& url)const
+//    {
+//        ASSERT_THAT(url, EndsWith(expectedURL));
+//    }
+//};
+
